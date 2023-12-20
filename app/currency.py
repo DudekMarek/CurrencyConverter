ccd07683a7deda5067a7879c230c7a63992e856a
DudekMarek <dudek371@gmail.com>
2023-12-20 17:55:05 +0100

    Initial commit

---
+++ app/currency.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+class Currency():
+    dolar_course: float
+
+    def __init__(self, dolar_course: float, name: str):
+        self.name = name
+        self.dolar_course = dolar_course
+
+    
+    def convert_to_dollar(self, quantity: float):
+        return round(quantity * self.dolar_course, 2)
+    
+    def convert_to_another_currency(self, another_currency: 'Currency', quantity: float):
+        dollar_equvivalent = self.convert_to_dollar(quantity=quantity)
+
+        return round(dollar_equvivalent / another_currency.dolar_course, 2)
---
+++ tests/test_currency.py
@@ -0,0 +1,34 @@
+import pytest
+from app.currency import Currency
+
+class TestCurrency:
+    
+    @pytest.fixture
+    def currency_instance(self):
+        return Currency(name="Currency", dolar_course=1.24)
+    
+    def test_creation(self, currency_instance):
+        obj = currency_instance
+
+        assert obj is not None
+
+    def test_attributes(self, currency_instance):
+        obj = currency_instance
+
+        assert obj.name == "Currency"
+        assert obj.dolar_course == 1.24
+
+    @pytest.mark.parametrize("currency, dollar", [(1, 1.24), (2, 2.48)])
+    def test_convert_to_dollar(self, currency_instance, currency, dollar):
+        obj = currency_instance
+
+        dolars_converted = obj.convert_to_dollar(currency)
+
+        assert dolars_converted == dollar
+
+    @pytest.mark.parametrize("quantity, currency_dollar_course ,converted_currency", [(1, 2, 0.62), (3.2, 0.87, 4.56)])
+    def test_convert_to_another_currency(self, currency_instance, quantity, currency_dollar_course ,converted_currency):
+        obj = currency_instance
+        converted_currency_instance = Currency(name="ConvertedCurrency", dolar_course=currency_dollar_course)
+
+        assert obj.convert_to_another_currency(another_currency=converted_currency_instance, quantity=quantity) == converted_currency
